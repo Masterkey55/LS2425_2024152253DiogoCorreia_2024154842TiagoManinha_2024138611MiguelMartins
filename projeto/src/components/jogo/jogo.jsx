1547b6a83e1c000e55edf2aeb6093193a4b3122f
Tiago Maninha <72321725+tiagomaninha@users.noreply.github.com>
2025-05-22 01:18:20 +0100

    posicoes dos slots corrigidos, responsivo agora

--- projeto/src/components/jogo/jogo.jsx
+++ projeto/src/components/jogo/jogo.jsx
@@ -4,12 +4,14 @@ import imagemTabuleiro from "../../assets/img/tabuleiro.png";
 
 function Jogo(props) {  
     return (
-        <div className="container-tabuleiro">
-            <img src={imagemTabuleiro} alt="Tabuleiro" className="tabuleiro" />
-            <div class = "slots-container">
-                {Array.from({ length: 49 }).map((_, i) => (
-                    <div key={i} className="slot"></div>
-                ))}
+        <div className="container-jogo">
+            <div className="container-tabuleiro">
+                <img src={imagemTabuleiro} alt="Tabuleiro" className="tabuleiro" />
+                <div class = "slots-container">
+                    {Array.from({ length: 42 }).map((_, i) => (
+                        <div key={i} className="slot"></div>
+                    ))}
+                </div>
             </div>
         </div>
     );
