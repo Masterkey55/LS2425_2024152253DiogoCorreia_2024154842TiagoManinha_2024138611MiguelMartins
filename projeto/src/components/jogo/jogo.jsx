ddccff4ed7c7b3a8febe4498e01974073ff3593f
Tiago Maninha <72321725+tiagomaninha@users.noreply.github.com>
2025-05-26 22:16:49 +0100

    Jogo quase feito

--- projeto/src/components/jogo/jogo.jsx
+++ projeto/src/components/jogo/jogo.jsx
@@ -12,16 +12,53 @@ function checkIfColunaVazia(coluna, discMatrix) {
     return false;
 }
 
+function checkIfVitoria(discMatrix) {
+    for (let row = 0; row < 6; row++) {
+        for (let col = 0; col < 7; col++) {
+            if (discMatrix[row][col] !== null) {
+                if (col + 3 < 7 &&
+                    discMatrix[row][col] === discMatrix[row][col + 1] &&
+                    discMatrix[row][col] === discMatrix[row][col + 2] &&
+                    discMatrix[row][col] === discMatrix[row][col + 3]) {
+                    return true;
+                }
+
+                if (row + 3 < 6 &&
+                    discMatrix[row][col] === discMatrix[row + 1][col] &&
+                    discMatrix[row][col] === discMatrix[row + 2][col] &&
+                    discMatrix[row][col] === discMatrix[row + 3][col]) {
+                    return true;
+                }
+                // diagonal /
+                if (row - 3 >= 0 && col + 3 < 7 &&
+                    discMatrix[row][col] === discMatrix[row - 1][col + 1] &&
+                    discMatrix[row][col] === discMatrix[row - 2][col + 2] &&
+                    discMatrix[row][col] === discMatrix[row - 3][col + 3]) {
+                    return true;
+                }
+                // diagonal \
+                if (row + 3 < 6 && col + 3 < 7 &&
+                    discMatrix[row][col] === discMatrix[row + 1][col + 1] &&
+                    discMatrix[row][col] === discMatrix[row + 2][col + 2] &&
+                    discMatrix[row][col] === discMatrix[row + 3][col + 3]) {
+                    return true;
+                }
+            }
+        }
+    }
+}
+
 function Jogo(props) {
     const [hoveredIndex, setHoveredIndex] = useState([]);
     const [currentPlayer, setCurrentPlayer] = useState("vermelho");
     const [gameOver, setGameOver] = useState(false);
+    const [topDisc, setTopDisc] = useState(0)
     const [discMatrix, setDiscMatrix] = useState(
                 Array.from({ length: 6 }, () => Array(7).fill(null))
             );
 
     const handleClick = (coluna) => {
-        if (checkIfColunaVazia(coluna, discMatrix)) {
+        if (checkIfColunaVazia(coluna, discMatrix) && !gameOver) {
             const newDiscMatrix = [...discMatrix];
             for (let i = 5; i >= 0; i--) {
                 if (newDiscMatrix[i][coluna] === null) {
@@ -30,6 +67,11 @@ function Jogo(props) {
                 }
             }
             setDiscMatrix(newDiscMatrix);
+
+            if (checkIfVitoria(discMatrix)) {
+                setGameOver(true)
+            }
+
             setCurrentPlayer(currentPlayer === "vermelho" ? "amarelo" : "vermelho");
         }
     }
@@ -39,34 +81,45 @@ function Jogo(props) {
         for (let i = 0; i < 6; i++) {
             indices.push(i * 7 + coluna);
         }
+        setTopDisc(coluna)
         setHoveredIndex(indices);
     };
 
     return (
-        <div className="container-jogo">
-            <div className="arrow-container"></div>
-            <div className="container-tabuleiro">
-                <img src={imagemTabuleiro} alt="Tabuleiro" className="tabuleiro" />
-                <div className="slots-container">
-                    {Array.from({ length: 6 }).map((_, row) => // linhas
-                        Array.from({ length: 7 }).map((_, col) => { // colunas
-                            const index = row * 7 + col;
-                            return (
-                                <Slot
-                                    key={index}
-                                    isHovered={hoveredIndex.includes(index)}
-                                    value={discMatrix[row][col]}
-                                    row={row}
-                                    col={col}
-                                />
-                            );
-                        })
-                    )}
-                </div>
-                <div class = "colunas">
-                    {Array.from({ length: 7 }).map((_, i) => (
-                        <div key={i} className="coluna" onClick={() => handleClick(i)} onMouseEnter={() => handleHover(i)} onMouseLeave={() => setHoveredIndex([])}></div>
-                    ))}
+        <div className={"container-jogo"}>
+            <div class="container-tabuleiro">
+                { !gameOver && 
+                    <div className="disc-container">
+                        <div className = {currentPlayer} 
+                            style={{
+                            left: `${topDisc * 14}%`
+                        }}
+                        ></div>
+                    </div>
+                }
+                <div className="tabuleiro">
+                    <img src={imagemTabuleiro} alt="Tabuleiro" className="tabuleiro-image" />
+                    <div className="slots-container">
+                        {Array.from({ length: 6 }).map((_, row) => // linhas
+                            Array.from({ length: 7 }).map((_, col) => { // colunas
+                                const index = row * 7 + col;
+                                return (
+                                    <Slot
+                                        key={index}
+                                        isHovered={hoveredIndex.includes(index)}
+                                        value={discMatrix[row][col]}
+                                        row={row}
+                                        col={col}
+                                    />
+                                );
+                            })
+                        )}
+                    </div>
+                    <div class = "colunas">
+                        {Array.from({ length: 7 }).map((_, i) => (
+                            <div key={i} className="coluna" onClick={() => handleClick(i)} onMouseEnter={() => handleHover(i)} onMouseLeave={() => setHoveredIndex([])}></div>
+                        ))}
+                    </div>
                 </div>
             </div>
         </div>
--- projeto/src/components/jogo/slot/slot.jsx
+++ projeto/src/components/jogo/slot/slot.jsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from "react";
+
 export default function Slot(props) {
     const { isHovered, value, row, col } = props;
+    let initialPosition = -(row + 1) * 100
+    const [position, setPosition] = useState(0);
+    let tempoAnimacao = (row + 1) / 2 * 0.11
     
+    useEffect(() => {
+        setPosition(initialPosition);
+        if (value) {
+            setPosition(0);
+        }
+    }, [value, initialPosition]);
+
     return (
         <div className={`slot ${isHovered ? "hovered" : ""}`}>
-            <div className={`disc ${value}`}></div>
+            {value && (
+                <div
+                    className={`disc ${value}`}
+                    style={{
+                        transition: `top ${tempoAnimacao}s linear`,
+                        top: `${position}%`
+                    }}
+                ></div>
+            )}
         </div>
     );
 }
